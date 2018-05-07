f4dedd01b1ba7a9571f34f10d3dce35a51dd3534
Lucas <lucas.pajares@gmail.com>
2018-05-07 14:31:29 +0200

    facebookAMedias

--- vue/frieza/src/components/LoginRegistro/LoginRegistro.component.js
+++ vue/frieza/src/components/LoginRegistro/LoginRegistro.component.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-
+var provider = new firebase.auth.FacebookAuthProvider();
 export default {
   name: 'login-registro',
   components: {},
@@ -25,7 +25,12 @@ export default {
   },
   clickDeBotonLogin:function(event){
     this.blLoginVisible=true;
-    this.sTitulo="Login";
+    this.sTitulo="Login Email";
+  },
+
+  clickDeBotonLoginT:function(event){
+    this.blLoginVisible=true;
+    this.sTitulo="Login Twitter";
   },
   registrar: function(event){
     firebase.auth().createUserWithEmailAndPassword(this.sEmail, this.sPassword).then(
@@ -37,7 +42,44 @@ export default {
       }
 );
 },
-  logear: function(event){
+
+   logear: function(event){
+    firebase.auth().signInWithEmailAndPassword(this.sEmail, this.sPassword).then(
+      function(user) {
+        alert("Estas dentro");
+      },
+      function(error){
+        alert(error);
+      }
+);
+},
+loguearF: function(event){
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});},
+clickDeBotonLogout: function(event){
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});}
+
+
+  /* Aqui mete el login de Twitter
+   logearT: function(event){
     firebase.auth().signInWithEmailAndPassword(this.sEmail, this.sPassword).then(
       function(user) {
         alert("Estas dentro");
@@ -47,5 +89,6 @@ export default {
       }
 );
   }
+  */
 }
 }
