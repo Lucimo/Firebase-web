2d5b9a33ef7eff5ab3f39f602c21c004b841209e
Primc <pablocruzmendez@gmail.com>
2018-05-14 14:10:08 +0200

    nombresDescarga

--- vue/frieza/src/components/Perfiles/Perfiles.component.js
+++ vue/frieza/src/components/Perfiles/Perfiles.component.js
@@ -1,13 +1,23 @@
 import { EventBus } from '../../Events/events_bus';
 import firebase from 'firebase'
 
+class Perfil{
+  constructor(id, datos){
+    this.id = id
+    this.name = datos.UserName
+    this.power = datos.NivelDePoder
+    this.range = datos.Rango
+    console.log("Nombre: "+this.name);
+  }
+}
+
 export default {
   name: 'perfiles',
   components: {},
   props: [],
   data () {
     return {
-
+      Perfil: []
     }
   },
   created: function(){
@@ -26,10 +36,12 @@ export default {
   },
   methods: {
     deacargarPerfiles: function(){
+      var that=this
       firebase.firestore().collection("Perfil").get().then(function(querySnapshot){
         querySnapshot.forEach(function(doc){
 
-          console.log(doc.id, " => ", doc.data());
+          //console.log(doc.id, " => ", doc.data());
+          that.Perfil.push(new Perfil(doc.id,doc.data()))
         });
       });
     }
